86972a58a8676d936252c67f0fe339eae2f28ba3
darklore <958690+darklore@users.noreply.github.com>
2026-07-25 01:00:10 +0900

    diffnav: add shell completion

--- Formula/d/diffnav.rb
+++ Formula/d/diffnav.rb
@@ -20,6 +20,7 @@ class Diffnav < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
+    generate_completions_from_executable(bin/"diffnav", shell_parameter_format: :cobra)
   end
 
   test do
